3b6f7fa07009bb35cf5fe913a4d64de15713cbad
Kim Yeojin <duwls1503@gmail.com>
2024-07-28 21:37:46 +0900

    feat: github pages build

---
+++ projects/projectData.ts
@@ -0,0 +1,8 @@
+import { ProjectDataType } from "../../src/types/type";
+import { project1 } from "./project1";
+import { project2 } from "./project2";
+import { project3 } from "./project3";
+import { project4 } from "./project4";
+import { project5 } from "./project5";
+
+export const projectData: ProjectDataType[] = [project1, project2, project3, project4, project5];
